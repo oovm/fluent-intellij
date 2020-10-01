b823a6bb1d05f1efbc81bead01b1089a7ab9561e
GitHub Action <action@github.com>
2020-10-01 08:42:00 +0800

    🔥 Template cleanup

--- src/main/kotlin/com/github/oovm/fluentintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/oovm/fluentintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.oovm.fluentintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.oovm.fluentintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
