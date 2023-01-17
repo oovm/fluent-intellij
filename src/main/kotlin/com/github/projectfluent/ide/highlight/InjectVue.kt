3d2aebf3f90f015b8adfc3db86495f6fc1b947d9
Aster <galaster@foxmail.com>
2023-01-17 12:14:57 +0800

    Try MultiHostInjector for vue

--- src/main/kotlin/com/github/projectfluent/ide/highlight/InjectVue.kt
+++ src/main/kotlin/com/github/projectfluent/ide/highlight/InjectVue.kt
@@ -1,13 +1,27 @@
 package com.github.projectfluent.ide.highlight
 
 import com.github.projectfluent.FluentLanguage
+import com.intellij.lang.injection.MultiHostInjector
+import com.intellij.lang.injection.MultiHostRegistrar
 import com.intellij.lang.injection.general.Injection
 import com.intellij.lang.injection.general.LanguageInjectionContributor
 import com.intellij.lang.injection.general.SimpleInjection
 import com.intellij.psi.PsiElement
+import com.intellij.psi.xml.XmlTag
 
-class InjectVue: LanguageInjectionContributor {
-    override fun getInjection(context: PsiElement): Injection? {
-        return SimpleInjection(FluentLanguage , "prefix", "suffix", "supportId" )
+class InjectVue : MultiHostInjector {
+    override fun getLanguagesToInject(registrar: MultiHostRegistrar, context: PsiElement) {
+        println("getLanguagesToInject: $context")
+        if (context is XmlTag) {
+            println("context: " + context.name)
+            if (context.name == "fluent") {
+                registrar.startInjecting(FluentLanguage)
+                registrar.doneInjecting()
+            }
+        }
+    }
+
+    override fun elementsToInjectIn(): MutableList<out Class<out PsiElement>> {
+        return mutableListOf(XmlTag::class.java)
     }
 }
