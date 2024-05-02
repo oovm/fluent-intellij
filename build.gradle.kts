ae13ec70f866a666d56a8f3f8b2bc52bbb1baba5
Aster <aster@vers.site>
2024-05-02 15:59:38 +0000

    🚀 Update to version 2024.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,58 +1,52 @@
+import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-fun properties(key: String) = project.findProperty(key).toString()
-
-plugins {
-    // Java support
-    id("java")
-    // Kotlin support
-    kotlin("jvm") version "1.7.0"
-    // Gradle IntelliJ Plugin
-    id("org.jetbrains.intellij") version "1.12.0"
-    // Gradle Changelog Plugin
-    id("org.jetbrains.changelog") version "1.3.1"
-    // Gradle Qodana Plugin
-    id("org.jetbrains.qodana") version "0.1.13"
+fun properties(key: String): Provider<String> {
+    return providers.gradleProperty(key)
 }
 
-group = properties("pluginGroup")
+fun environment(key: String) = providers.environmentVariable(key)
+
+plugins {
+    idea
+    java
+    kotlin("jvm") version "1.9.0"
+//    kotlin("plugin.serialization") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.17.2"
+    id("org.jetbrains.changelog") version "2.2.0"
+}
+
+group = properties("Group")
 version = properties("pluginVersion")
 
 // Configure project's dependencies
 repositories {
     mavenCentral()
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
 }
 
 sourceSets["main"].java.srcDirs("src/main/gen")
 
-// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
+    pluginName = properties("pluginName")
+    version = properties("platformVersion")
+    type = properties("platformType")
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
+    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
 changelog {
-    version.set(properties("pluginVersion"))
+    version = properties("pluginVersion")
     groups.set(emptyList())
-}
-
-// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
-qodana {
-    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
-    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
-    saveReport.set(true)
-    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
 }
 
 tasks {
     // Set the JVM compatibility versions
-    properties("javaVersion").let {
+    properties("javaVersion").get().let {
         withType<JavaCompile> {
             sourceCompatibility = it
             targetCompatibility = it
@@ -63,33 +57,39 @@ tasks {
     }
 
     wrapper {
-        gradleVersion = properties("gradleVersion")
+        gradleVersion = properties("gradleVersion").get()
     }
 
     patchPluginXml {
-        version.set(properties("pluginVersion"))
-        sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
+        version = properties("pluginVersion").get()
+        sinceBuild = properties("pluginSinceBuild").get()
+        untilBuild = properties("pluginUntilBuild").get()
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription.set(
-            projectDir.resolve("README.md").readText().lines().run {
-                val start = "<!-- Plugin description -->"
-                val end = "<!-- Plugin description end -->"
+        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
+            val start = "<!-- Plugin description -->"
+            val end = "<!-- Plugin description end -->"
 
+            with(it.lines()) {
                 if (!containsAll(listOf(start, end))) {
                     throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
                 }
-                subList(indexOf(start) + 1, indexOf(end))
-            }.joinToString("\n").run { markdownToHTML(this) }
-        )
+                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+            }
+        }
 
+        val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
-        changeNotes.set(provider {
-            changelog.run {
-                getOrNull(properties("pluginVersion")) ?: getLatest()
-            }.toHTML()
-        })
+        changeNotes = properties("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
     }
 
     // Configure UI tests plugin
@@ -109,21 +109,22 @@ tasks {
 
     publishPlugin {
         dependsOn("patchChangelog")
-        token.set(System.getenv("PUBLISH_TOKEN"))
-        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        token = environment("PUBLISH_TOKEN")
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+        channels = properties("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
     }
 }
-dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-}
+
+
+
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
-    jvmTarget = "17"
+    jvmTarget = properties("javaVersion").get()
 }
+
 val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
-    jvmTarget = "17"
+    jvmTarget = properties("javaVersion").get()
 }
--- src/main/kotlin/com/github/projectfluent/ide/actions/FluentConvertMo.kt
+++ src/main/kotlin/com/github/projectfluent/ide/actions/FluentConvertMo.kt
@@ -1,29 +1,29 @@
-package com.github.projectfluent.ide.actions
-
-import com.github.projectfluent.FluentBundle
-import com.github.projectfluent.language.file.FluentIcons.Companion.FILE
-import com.intellij.ide.actions.CreateFileAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.LangDataKeys
-import com.intellij.psi.PsiDirectory
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-
-class FluentConvertMo : CreateFileAction(name, description, FILE) {
-    companion object {
-        private val name = FluentBundle.message("action.convert_prop")
-        private val description = FluentBundle.message("action.convert_prop.description")
-
-    }
-
-    private var sourceFile: PsiFile? = null;
-
-    override fun update(event: AnActionEvent) {
-        sourceFile = LangDataKeys.PSI_FILE.getData(event.dataContext)
-        super.update(event)
-    }
-
-    override fun create(newName: String, directory: PsiDirectory): Array<PsiElement> {
-        return super.create(newName, directory)
-    }
-}
+//package com.github.projectfluent.ide.actions
+//
+//import com.github.projectfluent.FluentBundle
+//import com.github.projectfluent.language.file.FluentIcons.Companion.FILE
+//import com.intellij.ide.actions.CreateFileAction
+//import com.intellij.openapi.actionSystem.AnActionEvent
+//import com.intellij.openapi.actionSystem.LangDataKeys
+//import com.intellij.psi.PsiDirectory
+//import com.intellij.psi.PsiElement
+//import com.intellij.psi.PsiFile
+//
+//class FluentConvertMo : CreateFileAction(name, description, FILE) {
+//    companion object {
+//        private val name = FluentBundle.message("action.convert_prop")
+//        private val description = FluentBundle.message("action.convert_prop.description")
+//
+//    }
+//
+//    private var sourceFile: PsiFile? = null;
+//
+//    override fun update(event: AnActionEvent) {
+//        sourceFile = LangDataKeys.PSI_FILE.getData(event.dataContext)
+//        super.update(event)
+//    }
+//
+//    override fun create(newName: String, directory: PsiDirectory): Array<PsiElement> {
+//        return super.create(newName, directory)
+//    }
+//}
